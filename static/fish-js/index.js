b3a3c73eb8a2ad3b0cce6d6cc108903261dbda32
adaibin <adaibin201>
2017-01-08 23:23:34 +0800

    add function

--- static/fish-js/index.js
+++ static/fish-js/index.js
@@ -2,6 +2,25 @@
  * Created by binbin.zou on 2017/1/4.
  */
 
-$(document).ready(function () {
-    // alert("Welcome to my website.");
+
+$('#add').click(function (){
+    var value = $("#time").val();
+
+    if (!value){
+        return 0;
+    }
+
+    var list = $("#list");
+    if (list.children().length >= 10){
+        alert('当前提醒数已达到最大值.');
+        return 0;
+    }
+
+    list.append('<li class="item">' + value+ '<button class="remove" type="button"> x </button></li>');
+
+});
+
+
+$('.remove').click(function (){
+    alert('asx');
 });
