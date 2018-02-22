6a8dd1fe077aff3023956fccd585b40010b855f1
adaibin <yybzjn@163.com>
2018-02-22 15:14:42 +0800

    add nature

--- combinator.py
+++ combinator.py
@@ -7,11 +7,15 @@ researches = []
 page_path = os.getcwd() + '/static/pages'
 files = os.listdir(page_path)
 
+titles = []
 for file in files:
     with open(page_path + '/' + file, 'r', encoding='utf-8') as f:
         f_ = f.read()
         title = f_[f_.find('title>')+6:f_.find('</title')]
         id_ = file[:-5]
+        if title in titles:
+            continue
+        titles.append(title)
         researches.append({'id': id_, 'title': title})
 
 with open(os.getcwd() + '/nature_.html', 'r') as f:
