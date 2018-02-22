c309129989aeefe4a3c881b3204d9e9512c9c8c5
adaibin <yybzjn@163.com>
2018-02-22 16:41:40 +0800

    add nature

--- collector.py
+++ collector.py
@@ -5,37 +5,27 @@ import time
 
 import requests
 
-research_url = 'https://www.natureasia.com/zh-cn/life-sci/research/'
+research_url = 'https://www.natureasia.com/zh-cn/research/highlight/'
 review_url = 'https://www.natureasia.com/zh-cn/life-sci/review/'
 
 
 def get_(x):
     try:
-        resp = requests.get(''.join((research_url, str(x))), timeout=5)
-        if resp.status_code == 404:
-            raise ValueError('404')
+        resp = requests.get(''.join((research_url, str(x))), timeout=10)
+        print(resp.url, resp.status_code)
         return resp
     except Exception as _:
-        # try:
-        #     resp_ = requests.get(''.join((review_url, str(x))), timeout=5)
-        #     if resp_.status_code == 404:
-        #         raise ValueError('404')
-        #     return resp_
-        # except Exception as _:
-        #     print(x, 'both not exist')
+        print(x, _)
         return None
 
 
 def func_(x):
-    if os.path.isfile(os.getcwd() + '/pages/%s.html' % x):
+    if os.path.isfile(os.getcwd() + '/static/pages/%s.html' % x):
         return False
 
     resp = get_(x)
     if not resp:
         return False
-    # while not resp:
-    #     time.sleep(10)
-    #     resp = get_(x)
 
     title = re.findall('h1 class="title">(.*?)</h1', resp.text)
     title = title[0] if title else title
@@ -55,14 +45,11 @@ def func_(x):
     link = re.findall('href="(.*?)">原创文章</a>', resp.text)
     link = link[0] if link else link
 
-    if not date:
-        print(x, '404')
-        return False
     if not hl:
         print(x, 'no hl')
         return False
     with open(os.getcwd() + '/template.html', 'r') as f:
-
+        print(x, 'get')
         html_ = f.read()
         html_ = html_.format(title=title,
                              journal=journal,
@@ -70,10 +57,14 @@ def func_(x):
                              hl=hl,
                              doi=doi,
                              link=link)
-        with open(os.getcwd() + '/pages/%s.html' % x, 'w', encoding='utf-8') as wf:
+        with open(os.getcwd() + '/static/pages/%s.html' % x, 'w', encoding='utf-8') as wf:
             wf.write(html_)
 
 
-p = Pool(200)
-p.map(func_, range(16370, 21370))
+resp_ = requests.get('https://www.natureasia.com/zh-cn/research/', timeout=5)
+ids = re.findall('"/zh\-cn/research/\S*/(\d{1,5})"', resp_.text)
+
+p = Pool(50)
+p.map(func_, ids)
 p.close()
+p.join()
