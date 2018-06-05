f686a1312bad86228f910eb8744a9a0cec1778ea
adaibin <yybzjn@163.com>
2018-06-05 18:04:49 +0800

    add

--- dairy/qr_code.py
+++ dairy/qr_code.py
@@ -8,7 +8,7 @@ qr = qrcode.QRCode(version=7,
                    box_size=10,
                    border=30)
 
-qr.add_data('http://miss-fish.com/dairy/时雨.html')
+qr.add_data('http://miss-fish.com/dairy/flask.html')
 qr.make()
 img = qr.make_image(fill_color="black", back_color="white")
-img.save(open('时雨.png', 'wb'))
+img.save(open('flask.png', 'wb'))
